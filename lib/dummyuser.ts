252dd8af4dd560e72f290152ccbc8aa0dc21c3ca
rukunuddin29 <rukunuddin2003@gmail.com>
2026-06-03 17:26:54 +0530

    update

--- lib/dummyuser.ts
+++ lib/dummyuser.ts
@@ -3,14 +3,14 @@ export const users = [
     id: 1,
     email: "user@gmail.com",
     password: "123456",
-    role: "user",
+    role: "user" as const,
     name: "Regular User",
   },
   {
     id: 2,
     email: "host@gmail.com",
     password: "123456",
-    role: "host",
+    role: "host" as const,
     name: "Host User",
   },
 ];
